722f6a95e3a88ce0f7cdf6b9bc9a8537bebffc86
Sam Ojling <s.ojling@gmail.com>
2018-05-31 17:46:39 +0200

    Removed my default view file, got react up and running, created a sample page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
-  def index
+  def root
+    render html: '', layout: true
   end
 end
---
+++ frontend/javascript/components/hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hello = props => (
+  <div>
+    <h1>Hello {props.name}!</h1>
+    <p>Let's do this!</p>
+  </div>
+)
+export default Hello
