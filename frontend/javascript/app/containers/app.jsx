04ccab4bd879abf3d45f3702e9b4d8fa4bcbfd33
Sam Ojling <s.ojling@gmail.com>
2018-06-01 14:08:02 +0200

    Initilized redux layout, created basic folder structure and added the required packages

--- frontend/javascript/app/containers/app.jsx
+++ frontend/javascript/app/containers/app.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
+import { store } from '../store/reduxStore'
 import Hello from '../components/hello'
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Hello name='Sam' />
-      </div>
+      <Provider store={store}>
+        <div>
+          <Hello name='Sam' />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ frontend/javascript/app/store/reduxStore.jsx
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
